65e1e35979f8a131017aca641f9b6b6413615b39
Vipin M S <vipins2@vmware.com>
2022-11-10 14:00:03 -0600

    1668110401

--- hungryman-ui/src/DiningSearches.js
+++ hungryman-ui/src/DiningSearches.js
@@ -16,7 +16,7 @@ function DiningSearches(props)
         <div name="diningSearches" id="diningSearches">
           <div className="bannerHeader"> 
             <div className="bannerCenter">
-            <label className="searchesHeader">Submitted Searches And Results1668103201</label>
+            <label className="searchesHeader">Submitted Searches And Results1668110401</label>
             </div>      	
           </div>           
           {searches}
